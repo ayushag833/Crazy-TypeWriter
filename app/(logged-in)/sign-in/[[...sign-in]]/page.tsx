55e8f432d82f2859341df83b6b4e8a7d21bb8555
ayushaggarwal600 <ayushaggarwal600@gmail.com>
2024-10-22 23:13:19 +0530

    Project Completed

--- app/(logged-in)/sign-in/[[...sign-in]]/page.tsx
+++ app/(logged-in)/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,18 @@
 import BgGradient from "@/components/common/bg-gradient";
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <section className="flex justify-center items-center py-16">
+    <section className="flex justify-center items-center py-16 gap-[5rem]">
       <BgGradient>
         <SignIn />
       </BgGradient>
+      <Image src="/nature1.jpg" 
+       width={500}
+       height={500}
+       alt="Study Picture"
+       className="w-[55rem] h-[32rem] object-cover object-bottom rounded-lg"/>
     </section>
   );
 }
--- app/(logged-in)/sign-up/[[...sign-up]]/page.tsx
+++ app/(logged-in)/sign-up/[[...sign-up]]/page.tsx
@@ -1,12 +1,18 @@
 import BgGradient from "@/components/common/bg-gradient";
 import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <section className="flex justify-center items-center py-16">
+    <section className="flex justify-center items-center py-16 gap-[5rem]">
       <BgGradient>
         <SignUp />
       </BgGradient>
+      <Image src="/nature2.jpg" 
+       width={500}
+       height={500}
+       alt="Study Picture"
+       className="w-[55rem] h-[35rem] object-cover object-top rounded-lg"/>
     </section>
   );
 }
